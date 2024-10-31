3401baee6ef241f55b7022c5c17fe3c794197abd
Emilio Dalla Torre <info@emiliodallatorre.it>
2024-10-31 01:31:35 +0100

    Fix al nome al posto della mail

--- lib/resources/provider/signature_provider.dart
+++ lib/resources/provider/signature_provider.dart
@@ -16,7 +16,7 @@ class SignatureProvider {
 
     htmlSignature = htmlSignature.replaceAll("{{displayName}}", signature.displayName);
     htmlSignature = htmlSignature.replaceAll("{{role}}", signature.role);
-    htmlSignature = htmlSignature.replaceAll("{{email}}", signature.displayName);
+    htmlSignature = htmlSignature.replaceAll("{{email}}", signature.displayEmail);
     htmlSignature = htmlSignature.replaceAll("{{phoneNumber}}", signature.displayPhoneNumber);
 
     return htmlSignature;
